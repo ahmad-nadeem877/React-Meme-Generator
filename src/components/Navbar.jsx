81ed740ba3d83820f574936bb39f62fe1ea12a91
Ahmed <ahmadnadeem877@gmail.com>
2024-03-21 05:22:55 +0500

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar">
+        <img src="../../public/Logo.png" alt="logo-image" />
+        <p className="navbar-item">React course - Project 3</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
